7469db8f39c491031676da9a82379ce2fc73dea4
lytves <lytves@gmail.com>
2017-10-16 00:08:53 +0200

    complete SimpleChatBot

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # simple-telegram-bot
+
+Simple use of the ![Telegram Bot Java Library](https://github.com/rubenlagus/TelegramBots "Telegram Bot Java Library")
+
+For create your Telegram Bot is necesary send message "/start" to @BotFather.
+You will see all bots commands and send "/newbot" follow instructions of the @BotFather and give your bot unique name then receive a token.
+
+Put it all to the code and run your class with main method. Then your bot will work.
+
+The method "onUpdateReceived" will be called when Update is received by your bot.
--- src/main/java/com/companyname/telegram/bot/SimpleChatBot.java
+++ src/main/java/com/companyname/telegram/bot/SimpleChatBot.java
@@ -1,5 +1,58 @@
+package com.companyname.telegram.bot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
-public class SimpleChatBot {
+public class SimpleChatBot extends TelegramLongPollingBot{
+    public static void main(String[] args) {
+
+        //initialize API Context
+        ApiContextInitializer.init();
+
+        //Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            //register our bot
+            botsApi.registerBot(new SimpleChatBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public String getBotUsername() {
+        // your bot's name
+        return "your bot's name";
+    }
+
+    @Override
+    public String getBotToken() {
+        // // your bot's token
+        return "your bot's token";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        /*
+            Our bot resend a user's message to himself
+         */
+
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                sendMessage(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
